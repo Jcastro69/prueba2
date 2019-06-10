ad2d2222ee91a46ed627eac62e7a3be64193b9d6
Jcastro69 <51586455+Jcastro69@users.noreply.github.com>
2019-06-10 02:14:16 -0400

    Add files via upload

---
+++ maveExample/src/main/java/com/project/maveExample/App.java
@@ -0,0 +1,13 @@
+package com.project.maveExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
